cf9282ecd72d6187297e8391127878f330336267
Yi Wen <ywen@Zhan.local>
2009-11-27 23:57:43 -0600

    mv

---
+++ lib/action_web_service/string_to_datetime_for_soap.rb
@@ -0,0 +1,10 @@
+module StringToDatetimeForSoap
+  def to_datetime
+    if /^([+\-]?\d{4,})-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d(?:\.(\d*))?)(Z|(?:[+\-]\d\d:\d\d)?)?$/ =~ self.strip
+      return Time.xmlschema(self).localtime
+    end
+    super
+  end
+end
+
+String.send :include, StringToDatetimeForSoap
